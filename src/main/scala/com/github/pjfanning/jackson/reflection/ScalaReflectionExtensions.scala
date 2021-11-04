b61311919f181c75b6509c27287ac3d5477e35b6
PJ Fanning <pjfanning@users.noreply.github.com>
2021-11-05 00:53:32 +0100

    Update ScalaReflectionExtensions.scala

--- src/main/scala/com/github/pjfanning/jackson/reflection/ScalaReflectionExtensions.scala
+++ src/main/scala/com/github/pjfanning/jackson/reflection/ScalaReflectionExtensions.scala
@@ -1,12 +1,16 @@
 package com.github.pjfanning.jackson.reflection
 
 import co.blocke.scala_reflection.RType
-import co.blocke.scala_reflection.info.{ClassInfo, ScalaOptionInfo, SeqLikeInfo}
-import com.fasterxml.jackson.core.JsonParser
+import co.blocke.scala_reflection.info.{ClassInfo, MapLikeInfo, ScalaOptionInfo, SeqLikeInfo}
+import com.fasterxml.jackson.core.{JsonParser, TreeNode}
 import com.fasterxml.jackson.databind.json.JsonMapper
-import com.fasterxml.jackson.databind.{JavaType, ObjectMapper}
+import com.fasterxml.jackson.databind.{JavaType, MappingIterator, ObjectMapper, ObjectReader, ObjectWriter}
 import com.fasterxml.jackson.module.scala.{ClassTagExtensions, JavaTypeable}
-import com.fasterxml.jackson.module.scala.introspect.ScalaAnnotationIntrospector
+import _root_.com.fasterxml.jackson.module.scala.introspect.ScalaAnnotationIntrospector
+
+import java.io.{File, InputStream, Reader}
+import java.net.URL
+import scala.reflect.ClassTag
 
 object ScalaReflectionExtensions {
   def ::(o: JsonMapper) = new Mixin(o)
@@ -19,12 +23,169 @@ trait ScalaReflectionExtensions {
 
   private def registeredClasses = scala.collection.mutable.HashSet[Class[_]]()
 
-  def readValue[T: JavaTypeable](json: String): T = {
-    readValue(json, constructType[T])
-  }
-
+  /**
+   * Method to deserialize JSON content into a Java type, reference
+   * to which is passed as argument. Type is passed using so-called
+   * "super type token" (see )
+   * and specifically needs to be used if the root type is a
+   * parameterized (generic) container type.
+   */
   def readValue[T: JavaTypeable](jp: JsonParser): T = {
     readValue(jp, constructType[T])
+  }
+
+
+  /**
+   * Method for reading sequence of Objects from parser stream.
+   * Sequence can be either root-level "unwrapped" sequence (without surrounding
+   * JSON array), or a sequence contained in a JSON Array.
+   * In either case [[com.fasterxml.jackson.core.JsonParser]] must point to the first token of
+   * the first element, OR not point to any token (in which case it is advanced
+   * to the next token). This means, specifically, that for wrapped sequences,
+   * parser MUST NOT point to the surrounding <code>START_ARRAY</code> but rather
+   * to the token following it.
+   * <p>
+   * Note that [[com.fasterxml.jackson.databind.ObjectReader]] has more complete set of variants.
+   */
+  def readValues[T: JavaTypeable](jp: JsonParser): MappingIterator[T] = {
+    readValues(jp, constructType[T])
+  }
+
+  def treeToValue[T: JavaTypeable](n: TreeNode): T = {
+    treeToValue(n, constructType[T])
+  }
+
+  def readValue[T: JavaTypeable](src: File): T = {
+    readValue(src, constructType[T])
+  }
+
+  def readValue[T: JavaTypeable](src: URL): T = {
+    readValue(src, constructType[T])
+  }
+
+  def readValue[T: JavaTypeable](content: String): T = {
+    readValue(content, constructType[T])
+  }
+
+  def readValue[T: JavaTypeable](src: Reader): T = {
+    readValue(src, constructType[T])
+  }
+
+  def readValue[T: JavaTypeable](src: InputStream): T = {
+    readValue(src, constructType[T])
+  }
+
+  def readValue[T: JavaTypeable](src: Array[Byte]): T = {
+    readValue(src, constructType[T])
+  }
+
+  def readValue[T: JavaTypeable](src: Array[Byte], offset: Int, len: Int): T = {
+    readValue(src, offset, len, constructType[T])
+  }
+
+  def updateValue[T: JavaTypeable](valueToUpdate: T, src: File): T = {
+    objectReaderFor(valueToUpdate).readValue(src)
+  }
+
+  def updateValue[T: JavaTypeable](valueToUpdate: T, src: URL): T = {
+    objectReaderFor(valueToUpdate).readValue(src)
+  }
+
+  def updateValue[T: JavaTypeable](valueToUpdate: T, content: String): T = {
+    objectReaderFor(valueToUpdate).readValue(content)
+  }
+
+  def updateValue[T: JavaTypeable](valueToUpdate: T, src: Reader): T = {
+    objectReaderFor(valueToUpdate).readValue(src)
+  }
+
+  def updateValue[T: JavaTypeable](valueToUpdate: T, src: InputStream): T = {
+    objectReaderFor(valueToUpdate).readValue(src)
+  }
+
+  def updateValue[T: JavaTypeable](valueToUpdate: T, src: Array[Byte]): T = {
+    objectReaderFor(valueToUpdate).readValue(src)
+  }
+
+  def updateValue[T: JavaTypeable](valueToUpdate: T, src: Array[Byte], offset: Int, len: Int): T = {
+    objectReaderFor(valueToUpdate).readValue(src, offset, len)
+  }
+
+  private def objectReaderFor[T: JavaTypeable](valueToUpdate: T): ObjectReader = {
+    readerForUpdating(valueToUpdate).forType(constructType[T])
+  }
+
+  /*
+   **********************************************************
+   * Extended Public API: constructing ObjectWriters
+   * for more advanced configuration
+   **********************************************************
+   */
+
+  /**
+   * Factory method for constructing [[com.fasterxml.jackson.databind.ObjectWriter]] that will
+   * serialize objects using specified JSON View (filter).
+   */
+  def writerWithView[T: ClassTag]: ObjectWriter = {
+    writerWithView(classFor[T])
+  }
+
+  /**
+   * Factory method for constructing {@link com.fasterxml.jackson.databind.ObjectWriter} that will
+   * serialize objects using specified root type, instead of actual
+   * runtime type of value. Type must be a super-type of runtime type.
+   * <p>
+   * Main reason for using this method is performance, as writer is able
+   * to pre-fetch serializer to use before write, and if writer is used
+   * more than once this avoids addition per-value serializer lookups.
+   */
+  def writerFor[T: JavaTypeable]: ObjectWriter = {
+    writerFor(constructType[T])
+  }
+
+  /*
+   **********************************************************
+   * Extended Public API: constructing ObjectReaders
+   * for more advanced configuration
+   **********************************************************
+   */
+
+  /**
+   * Factory method for constructing [[com.fasterxml.jackson.databind.ObjectReader]] that will
+   * read or update instances of specified type
+   */
+  def readerFor[T: JavaTypeable]: ObjectReader = {
+    readerFor(constructType[T])
+  }
+
+  /**
+   * Factory method for constructing [[com.fasterxml.jackson.databind.ObjectReader]] that will
+   * deserialize objects using specified JSON View (filter).
+   */
+  def readerWithView[T: ClassTag]: ObjectReader = {
+    readerWithView(classFor[T])
+  }
+
+  /*
+   **********************************************************
+   * Extended Public API: convenience type conversion
+   **********************************************************
+   */
+
+  /**
+   * Convenience method for doing two-step conversion from given value, into
+   * instance of given value type. This is functionality equivalent to first
+   * serializing given value into JSON, then binding JSON data into value
+   * of given type, but may be executed without fully serializing into
+   * JSON. Same converters (serializers, deserializers) will be used as for
+   * data binding, meaning same object mapper configuration works.
+   *
+   * @throws IllegalArgumentException If conversion fails due to incompatible type;
+   *                                  if so, root cause will contain underlying checked exception data binding
+   *                                  functionality threw
+   */
+  def convertValue[T: JavaTypeable](fromValue: Any): T = {
+    convertValue(fromValue, constructType[T])
   }
 
   private def constructType[T: JavaTypeable]: JavaType = {
@@ -47,6 +208,8 @@ trait ScalaReflectionExtensions {
             ScalaAnnotationIntrospector.registerReferencedValueType(classInfo.infoClass, fieldInfo.name, optionInfo.optionParamType.infoClass)
           case seqInfo: SeqLikeInfo =>
             ScalaAnnotationIntrospector.registerReferencedValueType(classInfo.infoClass, fieldInfo.name, seqInfo.elementType.infoClass)
+          case mapInfo: MapLikeInfo =>
+            ScalaAnnotationIntrospector.registerReferencedValueType(classInfo.infoClass, fieldInfo.name, mapInfo.elementType2.infoClass)
           case _ =>
         }
         fieldInfo.fieldType match {
@@ -56,5 +219,9 @@ trait ScalaReflectionExtensions {
       }
       registeredClasses.add(classInfo.infoClass)
     }
+  }
+
+  private def classFor[T: ClassTag]: Class[T] = {
+    implicitly[ClassTag[T]].runtimeClass.asInstanceOf[Class[T]]
   }
 }
